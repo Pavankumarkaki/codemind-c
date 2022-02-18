b608c326320b1f2f9e09e70e868df45fcef17b90
pavankumarkaki <96739003+Pavankumarkaki@users.noreply.github.com>
2022-02-18 18:53:03 +0530

    #include<stdio.h> int main() {     int l,b,w,c,p,a,t;     scanf("%d%d%d%d",&l,&b,&w,&c);     l=l+(w);     b=b+(w);     p=2*(l+b);     a=p*w;     t=a*c;     printf("%d",t); }

---
+++ Cost_to_fence_the_park_outside.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int l,b,w,c,p,a,t;
+    scanf("%d%d%d%d",&l,&b,&w,&c);
+    l=l+(w);
+    b=b+(w);
+    p=2*(l+b);
+    a=p*w;
+    t=a*c;
+    printf("%d",t);
+}
