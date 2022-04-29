91b93ae35fe73b2a99b132a6a350086062e882a2
pavankumarkaki <96739003+Pavankumarkaki@users.noreply.github.com>
2022-04-30 00:49:04 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int fib[n]={0},i;     fib[0]=0,fib[1]=1;     for(i=2;i<n;i++)     {         fib[i]=fib[i-1]+fib[i-2];     }     for(i=0;i<n;i++)     printf("%d ",fib[i]); }

---
+++ fibonacci.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int fib[n]={0},i;
+    fib[0]=0,fib[1]=1;
+    for(i=2;i<n;i++)
+    {
+        fib[i]=fib[i-1]+fib[i-2];
+    }
+    for(i=0;i<n;i++)
+    printf("%d ",fib[i]);
+}
