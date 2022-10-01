ed10bfee743fd73619b790a30a5ae414704d57f4
pavankumarkaki <96739003+Pavankumarkaki@users.noreply.github.com>
2022-10-01 20:30:24 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n];     for(int i=0;i<n;i++)         scanf("%d",&arr[i]);     for(int i=0;i<n;i++)     {         int c=0;         for(int j=0;j<n;j++)         {             if(arr[i]==arr[j])                 c++;         }         if(c>(n/2))         {             printf("%d",arr[i]);             break;         }     } }

---
+++ Majority_Element.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+    for(int i=0;i<n;i++)
+    {
+        int c=0;
+        for(int j=0;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+                c++;
+        }
+        if(c>(n/2))
+        {
+            printf("%d",arr[i]);
+            break;
+        }
+    }
+}
